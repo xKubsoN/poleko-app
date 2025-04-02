f7e2160266f72831fa4c3afee9330abcdde97ad2
xKubsoN <kszczesny2555@gmail.com>
2025-04-02 18:10:08 +0200

    2-jsonfileread

--- src/main/java/com/example/xkubson/demoapp/MainApp.java
+++ src/main/java/com/example/xkubson/demoapp/MainApp.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 480);
         String css = this.getClass().getResource("style.css").toExternalForm();
         scene.getStylesheets().add(css);

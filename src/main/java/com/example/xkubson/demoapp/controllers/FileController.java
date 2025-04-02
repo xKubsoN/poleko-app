2f6d133d443d347ce74514499ba9af9bbfb1ad39
xKubsoN <kszczesny2555@gmail.com>
2025-04-03 01:13:53 +0200

    3-jsonfileread

--- src/main/java/com/example/xkubson/demoapp/controllers/FileController.java
+++ src/main/java/com/example/xkubson/demoapp/controllers/FileController.java
@@ -5,26 +5,32 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class FileController {
-    public static void main(String[] args) {
+    public List<String> getJsonKeys() {
+        List<String> keysList = new ArrayList<>();
+        List<String> valueList = new ArrayList<>();
         try {
             File JSONFile = new File("src/main/java/com/example/xkubson/demoapp/controllers/example_1.json");
             String content = Files.readString(JSONFile.toPath());
-            System.out.println(content);
+//            System.out.println(content); //DEBUG
             JSONObject jsonObject = new JSONObject(content);
 
             Iterator<String> keys = jsonObject.keys();
-            System.out.println("Keys:");
+//            System.out.println("Keys:"); //DEBUG
             while (keys.hasNext()) {
                 String key = keys.next();
-                Object value = jsonObject.get(key);
-                System.out.println(key + ": " + value);
+                keysList.add(key);
+//                Object value = jsonObject.get(key); //DEBUG
+//                System.out.println(key); //DEBUG
             }
         } catch (IOException e) {
             System.out.println("error");
             e.printStackTrace();
         }
+        return keysList;
     }
 }
